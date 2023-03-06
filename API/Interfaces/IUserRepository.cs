64f30b804b66dca3bab5f711c20d6093bacf2824
Atanas Shivachev <atanas.shivachev@dorchestercollection.com>
2023-03-06 10:46:36 +0200

    End of section 8

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser appUser);
+
+        Task<bool> SaveAllAsync();
+
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+
+        Task<AppUser> GetUserByIdAsync(int id);
+
+        Task<AppUser> GetUserByNameAsync(string username);
+
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+}
