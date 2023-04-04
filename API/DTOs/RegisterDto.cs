fd5e2cd3d8e5bb53050f6746355f15561fe9a5d9
Atanas Shivachev <atanas.shivachev@dorchestercollection.com>
2023-04-04 17:31:38 +0300

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,8 +4,15 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
-        public string Username { get; set; }
+        [Required] public string Username { get; set; }
+
+        [Required] public string KnownAs { get; set; }
+
+        [Required] public DateOnly? DateOfBirth { get; set; }
+
+        [Required] public string City { get; set; }
+
+        [Required] public string Country { get; set; }
 
         [Required]
         [StringLength(8, MinimumLength = 4)]
